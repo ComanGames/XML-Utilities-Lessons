65f312a647290f3876a7016a77b3976d27572db1
ComanGames <alfaa.gen@gmail.com>
2016-02-03 12:55:54 -0500

    XMLUtilitities

---
+++ XML-Utilities-Lessons/Qustion.cs
@@ -0,0 +1,19 @@
+
+namespace XML_Utilities_Lessons
+{
+	public class Qustion
+	{
+		public string Name;
+		public string Text;
+
+		public Qustion(string name, string text)
+		{
+			Name = name;
+			Text = text;
+		}
+
+		public Qustion()
+		{
+		}
+	}
+}
---
+++ XML-Utilities-Lessons/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace XML_Utilities_Lessons
+{
+	[Serializable]
+    public class Test
+	{
+		public int Number;
+		public string Name;
+		public int TimeInMinutes;
+		public Qustion[] Qustions;
+
+		public Test()
+		{
+		}
+
+		public Test(int number, string name, int timeInMinutes, Qustion[] qustions)
+		{
+			TimeInMinutes = timeInMinutes;
+			Number = number;
+			Name = name;
+			Qustions = qustions;
+		}
+
+	}
+}
